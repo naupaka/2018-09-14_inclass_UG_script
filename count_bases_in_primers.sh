090f923db4ec96a83c5b7f5f05f9902891696865
Naupaka Zimmerman <naupaka@gmail.com>
2018-09-14 15:21:24 -0700

    Script works! Fixed for loop output

--- count_bases_in_primers.sh
+++ count_bases_in_primers.sh
@@ -19,5 +19,7 @@
 # head -2 primer_B.fasta | tail -1 | tr -d '\n' | wc -m
 for file in "$@"
 do
-  expr $(head -2 $file | tail -1 | wc -m) - 1
+  FILENAME=$(basename $file .fasta)
+  COUNT=$(expr $(head -2 $file | tail -1 | wc -m) - 1)
+  echo In $FILENAME, there are $COUNT nucleotides.
 done
