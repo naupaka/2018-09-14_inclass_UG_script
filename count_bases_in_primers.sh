5d21315905cc41b3b2a7b7e215403c2eda8f16c5
Naupaka Zimmerman <naupaka@gmail.com>
2018-09-14 15:59:09 -0700

    Script passes all shellcheck recs and runs!

--- count_bases_in_primers.sh
+++ count_bases_in_primers.sh
@@ -19,7 +19,7 @@
 # head -2 primer_B.fasta | tail -1 | tr -d '\n' | wc -m
 for file in "$@"
 do
-  FILENAME=$(basename $file .fasta)
-  COUNT=$(expr $(head -2 $file | tail -1 | wc -m) - 1)
-  echo In $FILENAME, there are $COUNT nucleotides.
+  FILENAME=$(basename "$file" .fasta)
+  COUNT=$(head -2 "$file" | tail -1 | tr -d '\n' | wc -m | tr -d " ")
+  echo In "$FILENAME", there are "$COUNT" nucleotides.
 done
